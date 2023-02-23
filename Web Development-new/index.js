78b89645cb02db8078ae9d5e09d11eb1279e99e7
Kunal Mishra <90129169+kunalmishraa@users.noreply.github.com>
2023-02-23 21:30:21 +0530

    Add files via upload

---
+++ Web Development-new/index.js
@@ -0,0 +1,88 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([[12], {
+    340: function(e) {
+        e.exports = JSON.parse('{"birthday":"1988-09-30T00:00:00.000+07:00","interview":false,"salary":{"base":75118,"expect":125000,"rate":34.44,"day":30,"hour":8,"currency":"$"},"coding":{"top5":[{"caption":"JavaScript","commits":3941,"loc":782828},{"caption":"CSS","commits":1247,"loc":751121},{"caption":"HTML","commits":670,"loc":343673},{"caption":"C#","commits":349,"loc":1391018},{"caption":"Shell","commits":307,"loc":390},{"caption":"SQL","commits":156,"loc":81635}],"best_languages":"Vue.js","day_of_year":315},"en":{"fullname":"Kananek Thongkam","national":"Thai","religion":"Buddhist","national_id":"1860800038291","location":"Bangkok, Sathorn","language":{"thai":"Native","english":"Basic"},"job":"Technology Developer","detail":"Hi, I\'m KEM. I live in Bangkok Thailand, I graduated with a bachelor of science in Information Technology from Phetchaburi Rajabhat University, and I would like a job that allows for personal and professional growth.","expertise":[{"subject":"Frontend developer","detail":"Specializing in using Frameworks like `Nuxt.js`, `Next.js` and `Astro` to create Web applications."},{"subject":"Backend developer","detail":"Programming on `Golang`, `Nodejs`, `Python` and security is extremely important."},{"subject":"DevOps","detail":"I can manage both `Windows` and `Linux` Server and ability to use `docker`, `k8s`, or cloud platform `aws`, `gcloud`, `azure`,`digital ocean`"},{"subject":"Solution architecture","detail":"Expertise in infrastructure design with `container` and `microservice`."}],"work":[{"range":{"begin":"2022-11-01T00:00:00.000+07:00","quit":null},"job":"Technology Developer","work":"SCG Digital Office, Bang Sue.","file":"2022-work-scg-do.md"},{"pagebreak":true},{"range":{"begin":"2018-02-01T00:00:00.000+07:00","quit":"2022-10-31T23:59:59.999+07:00"},"work":"CENTRALGROUP, RIS Company Limited, Bang Rak","jobs":[{"range":{"begin":"2019-02-01T00:00:00.000+07:00","quit":"2022-10-31T23:59:59.999+07:00"},"job":"Senior Full Stack Engineer","file":"2018-work-central-group-2.md"},{"range":{"begin":"2018-02-01T00:00:00.000+07:00","quit":"2019-02-01T23:59:59.999+07:00"},"job":"Senior Development asst. Division Manager","file":"2018-work-central-group-1.md"}]},{"pagebreak":true},{"range":{"begin":"2013-04-29T00:00:00.000+07:00","quit":"2018-12-31T23:59:59.999+07:00"},"job":"Full Stack Developer","work":"Nippon SySits Co.,Ltd, Sathorn.","file":"2013-work-nippon-sysits.md"},{"range":{"begin":"2011-04-28T00:00:00.000+07:00","quit":"2013-04-28T23:59:59.999+07:00"},"job":"Freelance — Programmer","work":"Phetchaburi","file":"2011-work-freelance.md"}],"education":[{"range":"2007 - 2011","major":"Bachelor of Science (B.S.)","location":"Phetchaburi Rajabhat University, Phetchaburi.","branch":"Information Technology."},{"range":"2005 - 2007","major":"Vocational Certificate","location":"Chumphon Polytechnic College, Chumphon.","branch":"Business Computer"},{"range":"2003 - 2005","major":"Grade 7-9","location":"Sawi Witthaya School, Sawi, Chumphon.","branch":"High school"},{"range":"1998 - 2003","major":"Grade 1-6","location":"Barnnapoo school, Sawi, Chumphon.","branch":"Elementary school"}]},"th":{"fullname":"กัณฑ์อเนก ทองคำ","national":"ประเทศไทย","location":"กรุงเทพฯ สาทร","language":"ไทย <span>(หลัก)</span>, อังกฤษ <span>(พื้นฐาน)</span>","job":"Technology Developer","detail":"ฉันรักการเขียนโปรแกรมและเทคโนโลยีใหม่ ๆ ฉันสามารถเพิ่มความสามารถในการพัฒนาตัวเองได้ตลอดเวลา ฉันจะมีความสุขมาก ถ้าซอฟต์แวร์สามารถพัฒนาให้ผู้ใช้สามารถทำงานได้ราบรื่นยิ่งขึ้น","expertise":[{"subject":"ผู้พัฒนาส่วนหน้า","detail":"เชี่ยวชาญในการใช้ Framework เช่น `Nuxt.js`, `Next.js`, MVC เพื่อสร้างเว็บแอปพลิเคชัน"},{"subject":"ผู้พัฒนาแบ็กเอนด์","detail":"การเขียนโปรแกรมบน \'Golang\', \'Rustlang\', \'Python\', \'Nodejs\' และ \'C#\' .NET Framework เพื่อความปลอดภัยเป็นสิ่งสำคัญอย่างยิ่ง"},{"subject":"ผู้ดูแลเซิร์ฟเวอร์","detail":"ฉันสามารถจัดการทั้งเซิร์ฟเวอร์ `Windows` และ `Linux` และความสามารถในการใช้ `docker`, `k8s`, `k3s`, `aws`, `gcloud`, `azure`"},{"subject":"DevOps","detail":"เชี่ยวชาญในการออกแบบโครงสร้างพื้นฐานด้วย `คอนเทนเนอร์` และ `ไมโครเซอร์วิส`"}],"work":[{"range":{"begin":"2018-02-01T00:00:00.000+07:00","quit":null},"work":"CENTRALGroup, RIS Co.,Ltd., บางรัก","jobs":[{"range":{"begin":"2019-01-01T00:00:00.000+07:00","quit":null},"job":"วิศวกรเต็มกอง","file":"2018-work-central-group-2.md"},{"range":{"begin":"2018-02-01T00:00:00.000+07:00","จบ":"2019-01-01T00:00:00.000+07:00"},"job":"ผู้ช่วยผู้จัดการฝ่ายพัฒนาอาวุโส","file":"2018-work-central-group-1.md"}]},{"pagebreak":true},{"range":{"begin":"2013-03-28T00:00:00.000+07:00","quit":"2018-12-31T23:59:59.999+07:00"},"job":"ผู้พัฒนาเต็มกอง","work":"บริษัท นิปปอน ซิสซิท บจก. สาทร","file":"2013-work-nippon-sysits.md"},{"range":{"begin":"2011-03-30T00:00:00.000+07:00","quit":"2013-03-27T00:00:00.000+07:00"},"job":"ฟรีแลนซ์ — โปรแกรมเมอร์","work":"เพชรบุรี","file":"2011-work-freelance.md"}],"education":[{"range":"2007 - 2011","major":"วิทยาศาสตรบัณฑิต (วท.บ.)","location":"มหาวิทยาลัยราชภัฏเพชรบุรี เพชรบุรี.","branch":"เทคโนโลยีสารสนเทศ"},{"range":"2005 - 2007","major":"ปวช.","location":"วิทยาลัยสารพัดช่างชุมพร ชุมพร.","branch":"ธุรกิจคอมพิวเตอร์"},{"range":"2003 - 2005","major":"ป.7-9","location":"โรงเรียนสวีวิทยา สวี ชุมพร.","branch":"มัธยมศึกษาตอนปลาย"},{"range":"2541 - 2546","major":"ป.1-6","location":"โรงเรียนพรรณภู สวี ชุมพร","branch":"ประถมศึกษา"}]},"social":[{"name":"github","icon":["fab","github"],"link":"https://touno.io/s/w9z5","alt":"github.com"},{"name":"linkedin","icon":["fab","linkedin"],"link":"https://touno.io/s/ig6h","alt":"linkedin"},{"name":"wakatime","icon":["fas","clock"],"link":"https://touno.io/s/7y9r","alt":"wakatime.com"},{"name":"community","icon":["fab","codepen"],"link":"https://touno.io/s/ixj7","alt":"discord.com"}],"skill":{"coding":["Golang","Nodejs","Python","Dart","React","Vue.js","SCSS","SecureShell","K8S-YAML","Dockerfile","ES2022","C#","VB"],"tools":["Git","VSCode","Visual Studio","Kubernates","Putty","Docker","Postman","Nuxtjs","Nextjs","Astro","Bootstrap","Elctronjs"],"SQL & NOSQL":["PostgreSQL","Redis","MongoDB","Influx","MySQL","Oracle","MSSQL","SQLite"],"OS & Cloud":["CentOS","Debian","Ubuntu","AWS","GCloud","Azure"]},"contact":[{"print":false,"label":"INSTRGRAM","url":"https://touno.io/s/u01t","text":"Kananek Thongkam","icon":["fab","instagram"]},{"print":false,"label":"FACEBOOK","url":"https://touno.io/s/zo2h","text":"Kananek Thongkam","icon":["fab","facebook-f"]},{"print":false,"label":"TWITTER","url":"https://touno.io/s/8ae4","text":"Kananek T.","icon":["fab","twitter"]},{"print":false,"label":"EMAIL","url":"mailto:kananek.dev@touno.io","text":"kananek.dev@touno.io","icon":["fas","envelope"]},{"print":true,"label":"LINE ACCOUNT","qrcode":"/qr_line.webp","url":"https://touno.io/s/5sri","text":"dvgamer (KEM)","icon":["fab","line"]},{"print":true,"label":"Website","qrcode":"/qr_website.webp","url":"https://touno.io/s/rfd6","text":"dvgamer (KEM)","icon":["fab","chrome"]},{"print":true,"label":"MOBILE","url":"tel:0970347607","text":"+66-970-3476-07","icon":["fas","mobile"]},{"print":true,"label":"EMAIL","url":"mailto:kananek.dev@touno.io","text":"kananek.dev@touno.io","icon":["fas","envelope"]}],"portfolio":[{"url":"","thumb":"portfolio-8.jpg","title":"TonSone GameRoom (2016)","subtitle":"design logo"},{"url":"","thumb":"portfolio-9.jpg","title":"deBUGerr (2014)","subtitle":"design logo"},{"url":"","thumb":"portfolio-7.jpg","title":"HaKkoMEw (2003)","subtitle":"design logo"},{"url":"","thumb":"portfolio-3.jpg","title":"Hentai Downloader (2017)","subtitle":"software"},{"url":"https://mos.travox.com/","thumb":"portfolio-2.jpg","title":"Travox MidBack (2014)","subtitle":"mos.travox.com"},{"url":"http://raannuch.co.uk","thumb":"portfolio-1.jpg","title":"Raannuch (2012)","subtitle":"raannuch.co.uk"},{"url":"","thumb":"portfolio-5.jpg","title":"Secure Stock (2015)","subtitle":"stock-online"},{"url":"","thumb":"portfolio-4.jpg","title":"Student Timeline (2007)","subtitle":"university-project"},{"url":"","thumb":"portfolio-6.jpg","title":"PSP Library (2005)","subtitle":"software"}],"certificate":[{"icon":"cert/exam-badge.jpg","cert":"/cert/file/Microsoft_Certified_Professional_Certificate_0.pdf","exam":"Microsoft Specialist: Programming in HTML5 with JavaScript and CSS3","achievement":"2016-06-13T00:00:00.000+07:00","detail":""},{"icon":"cert/exam-badge.jpg","cert":"/cert/file/Microsoft_Certified_Professional_Certificate_3.pdf","exam":"Microsoft Certified Professional: Microsoft Certified Professional","achievement":"2016-06-15T00:00:00.000+07:00","detail":""},{"icon":"cert/mcds-badge.jpg","cert":"/cert/file/Microsoft_Certified_Professional_Certificate_1.pdf","exam":"Microsoft Certified Solutions Developer: Web Applications","achievement":"2016-06-20T00:00:00.000+07:00","detail":""},{"icon":"cert/mcds-badge.jpg","cert":"/cert/file/Microsoft_Certified_Professional_Certificate_4.pdf","exam":"Microsoft Certified Solutions Developer: App Builder","achievement":"2016-09-25T00:00:00.000+07:00","detail":""},{"icon":"cert/mcsa-badge.jpg","cert":"/cert/file/Microsoft_Certified_Professional_Certificate_2.pdf","exam":"Microsoft Certified Solutions Associate: Web Applications","achievement":"2016-09-25T00:00:00.000+07:00","detail":""}],"wallet":{"scb":"4088294384","bbl":"0867214769","kbank":"1128052360","btc":"bc1qeu852pqaphk0f39ult4f4uxq3kuy9pevl0mjjc","eth":"0xe91bf27141b5f827cd1e164d0284a3cec1145d31","bnb":"bnb1g7kg0yl50u5xj5kumga24uudc5204pzgxkgg4u","bnb_tag":"1064894661"},"donate":{"content":"**Thank you everyone for supporting me**,  whether donations. \\n\\nArticle publishing Sharing information with me This is the kind of help that I would like to thank everyone very much.\\n\\nOpen source software of the developer. No policy to charge. Or any trading. To access add-ons in the developer\'s software or applications. The donation to the developer directly is only a user\'s choice."}}')
+    },
+    358: function(e, t, o) {
+        var content = o(383);
+        content.__esModule && (content = content.default),
+        "string" == typeof content && (content = [[e.i, content, ""]]),
+        content.locals && (e.exports = content.locals);
+        (0,
+        o(60).default)("46c83dc0", content, !0, {
+            sourceMap: !1
+        })
+    },
+    382: function(e, t, o) {
+        "use strict";
+        o(358)
+    },
+    383: function(e, t, o) {
+        var n = o(59)(!1);
+        n.push([e.i, '.skill-list h3[data-v-45f87cb7]{text-transform:uppercase}.skill-list li[data-v-45f87cb7]{display:inline-block;padding-right:4px;font-size:.85rem}.skill-list li[data-v-45f87cb7]:after{content:","}.skill-list li[data-v-45f87cb7]:last-child:after{content:""!important}.progress-item[data-v-45f87cb7]{position:relative}.progress-item .progress-title[data-v-45f87cb7]{font-size:12px;font-weight:400;display:inline-block;margin-bottom:5px}.progress-item .progress[data-v-45f87cb7]{height:2px;box-shadow:none;border-radius:0;background:transparent}.progress-item .progress-bar[data-v-45f87cb7]{background-color:#ff5722;box-shadow:none;text-align:right}.progress-item .progress-percent[data-v-45f87cb7]{font-size:10px;background-color:#313131;position:absolute;top:5px;padding:0 8px;border-radius:3px}.progress-item .progress-percent[data-v-45f87cb7]:before{content:"";position:absolute;left:0;bottom:-4px;border-top:6px solid #313131;border-right:8px solid transparent}@media (max-width:768px){.progress-wrapper[data-v-45f87cb7]{margin-bottom:50px}}', ""]),
+        e.exports = n
+    },
+    402: function(e, t, o) {
+        "use strict";
+        o.r(t);
+        o(15);
+        var n = o(340)
+          , r = {
+            data: function() {
+                return {
+                    skill: n.skill
+                }
+            },
+            methods: {
+                skillHeader: function() {
+                    return Object.keys(this.skill)
+                }
+            }
+        }
+          , l = (o(382),
+        o(48))
+          , component = Object(l.a)(r, (function() {
+            var e = this
+              , t = e._self._c;
+            return t("section", {
+                staticClass: "section-wrapper section-skills"
+            }, [t("div", {
+                staticClass: "row"
+            }, [e._m(0), e._v(" "), t("div", {
+                staticClass: "col-lg-27 col-md-25 col-sm-36"
+            }, [t("div", {
+                staticClass: "row"
+            }, e._l(e.skillHeader(), (function(o, i) {
+                return t("div", {
+                    key: i,
+                    staticClass: "col-md-18 skill-list"
+                }, [t("h3", {
+                    staticClass: "mb-1",
+                    domProps: {
+                        textContent: e._s(o)
+                    }
+                }), e._v(" "), t("ul", {
+                    staticClass: "pb-3"
+                }, e._l(e.skill[o], (function(o, n) {
+                    return t("li", {
+                        key: n,
+                        domProps: {
+                            textContent: e._s(o)
+                        }
+                    })
+                }
+                )), 0)])
+            }
+            )), 0)])])])
+        }
+        ), [function() {
+            var e = this._self._c;
+            return e("div", {
+                staticClass: "col-lg-9 col-md-11"
+            }, [e("div", {
+                staticClass: "section-title"
+            }, [e("h2", [this._v("Skills")])])])
+        }
+        ], !1, null, "45f87cb7", null);
+        t.default = component.exports
+    }
+}]);
